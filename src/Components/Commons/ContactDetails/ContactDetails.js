3828c393907c49d535e1edc08c5b738e1119b3d8
Sahil26299 <sahil.lokhande@indicchain.com>
2023-02-07 06:50:01 +0530

    Contact details UI completed

--- src/Components/Commons/ContactDetails/ContactDetails.js
+++ src/Components/Commons/ContactDetails/ContactDetails.js
@@ -1,26 +1,83 @@
-import React, {useContext} from 'react'
+import React, {useContext, useRef} from 'react'
 import { ColorSchema } from '../../../Utils/Context/ColorThemes';
 import './ContactDetails.css'
 import TabHeader from '../TabHeader'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import { Call } from '@mui/icons-material';
+import { Call, Mail, LinkedIn, GitHub, SendRounded, AccountCircle, Message } from '@mui/icons-material';
+import emailjs from '@emailjs/browser';
 
 export default function ContactDetails() {
-    const Colors = useContext(ColorSchema)
+    const Colors = useContext(ColorSchema);
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
   return (
     <Box className='ContactWrapper' >
         <TabHeader Children={'Contact Details'} Animation={'ContactHeader'} />
-        <Grid container spacing={2}>
-            <Grid item lg={2} sm={12} sx={{color:Colors.newVar.TXTColor, display:'flex', alignItems:'center', justifyContent:'center'}} >
+        <Grid container spacing={4}>
+            <Grid item lg={2} sm={12} sx={{color:Colors.newVar.TXTColor, display:'flex', alignItems:'center', justifyContent:'flex-end', }} >
                 <a href="" className={'CallMeIcon'}>
                     <Call className='CallIcon'/>
                 </a>
             </Grid>
-            <Grid item lg={8} sm={12} >
+            <Grid item lg={10} sm={12} >
                 <span style={{color:Colors.newVar.TXTColor}} className={'CallMeText'} >+91 915098814</span>
             </Grid>
+            <Grid item lg={2} sm={12} sx={{color:Colors.newVar.TXTColor, display:'flex', alignItems:'center', justifyContent:'flex-end', }} >
+                <a href="mailto:sahillokhande94@gmail.com" className={'CallMeIcon'}>
+                    <Mail className='MailIcon'/>
+                </a>
+            </Grid>
+            <Grid item lg={10} sm={12} >
+                <span style={{color:Colors.newVar.TXTColor}} className={'MailMeText'} >sahillokhande94@gmail.com</span>
+            </Grid>
+            <Grid item lg={2} sm={12} sx={{color:Colors.newVar.TXTColor, display:'flex', alignItems:'center', justifyContent:'flex-end', }} >
+                <a href="https://www.linkedin.com/in/sahillokhande26" target={'_blank'} className={'CallMeIcon'}>
+                    <LinkedIn className='MailIcon'/>
+                </a>
+            </Grid>
+            <Grid item lg={10} sm={12} >
+                <span style={{color:Colors.newVar.TXTColor}} className={'MailMeText'} >sahillokhande26</span>
+            </Grid>
+            <Grid item lg={2} sm={12} sx={{color:Colors.newVar.TXTColor, display:'flex', alignItems:'center', justifyContent:'flex-end', }} >
+                <a href="https://github.com/Sahil26299" target={'_blank'} className={'CallMeIcon'}>
+                    <GitHub className='MailIcon'/>
+                </a>
+            </Grid>
+            <Grid item lg={10} sm={12} >
+                <span style={{color:Colors.newVar.TXTColor}} className={'MailMeText'} >Github Link</span>
+            </Grid>
         </Grid>
+        <div style={{marginTop:'5%', paddingBottom:'5%', marginLeft:'10%'}} >
+            <form action="" ref={form} onSubmit={sendEmail}>
+                <label htmlFor="name" id='emailLabel' style={{color:Colors.newVar.TXTColor}} >Write a message:</label>
+                <div id={'emailInputdiv'}>
+                    <input type={'text'} id={'name'} style={{color:Colors.newVar.TXTColor}} maxLength={60} placeholder={'Your name'} />
+                    <AccountCircle style={{color:Colors.newVar.TXTColor, fontSize:30}} />
+                </div>
+                <div id={'messageInputdiv'}>
+                    <textarea id='message' placeholder='Write a message...' maxLength={500} style={{color:Colors.newVar.TXTColor}} rows={5} cols={5} />
+                    <Message style={{color:Colors.newVar.TXTColor, fontSize:30}} />
+                </div>
+                <div id={'emailInputdiv'}>
+                    <input type={'email'} id={'email'} style={{color:Colors.newVar.TXTColor}} maxLength={256} placeholder={'Your email'} />
+                    <Mail style={{color:Colors.newVar.TXTColor, fontSize:30}} />
+                </div>
+                <button type="submit" style={{color:Colors.newVar.TXTColor, backgroundColor:'orange', borderColor:'transparent', padding:10, borderRadius:5, marginTop:'3%'}}>
+                    <SendRounded id={'sendIcon'} />
+                </button>
+            </form>
+        </div>
     </Box>
   )
 }
--- src/Components/Commons/TabHeader.js
+++ src/Components/Commons/TabHeader.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Box } from '@mui/material'
+import {ColorSchema} from '../../Utils/Context/ColorThemes'
 
 export default function TabHeader({ Children, Animation}) {
+    const Colors = useContext(ColorSchema);
     return (
         <Box sx={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '25%', marginBottom: '5%', alignItems: 'center' }} >
-            <div className='horizontalLine1' style={{ backgroundColor: 'rgba(255,255,255,0.25)', height: 0.75, width: '30%' }} />
+            <div className='horizontalLine1' style={{ backgroundColor: Colors.isDarkMode ? 'rgba(255,255,255,0.25)' : 'rgba(0,0,0,0.25)', height: 0.75, width: '30%' }} />
             <span className={`ServicesHeading ${Animation}`} style={{ color: 'orange' }} >{Children}</span>
-            <div className='horizontalLine2' style={{ backgroundColor: 'rgba(255,255,255,0.25)', height: 0.75, width: '30%' }} />
+            <div className='horizontalLine2' style={{ backgroundColor:Colors.isDarkMode ? 'rgba(255,255,255,0.25)' : 'rgba(0,0,0,0.25)', height: 0.75, width: '30%' }} />
         </Box>
     )
 }
